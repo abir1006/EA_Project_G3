e4ab571fb7f63b74c791ea8225b1f16e32b87aeb
Binod Rasaili <binod.toyam@gmail.com>
2024-07-16 16:52:08 -0500

    initial done

--- demo-app-post/src/main/java/app/demoapppost/TwitApplication.java
+++ demo-app-post/src/main/java/app/demoapppost/TwitApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoAppPostApplication {
+public class TwitApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoAppPostApplication.class, args);
+        SpringApplication.run(TwitApplication.class, args);
     }
 
 }
