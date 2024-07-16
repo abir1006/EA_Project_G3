98225145b1005b30878302055b4fae0fcef87f4f
Binod Rasaili <binod.toyam@gmail.com>
2024-07-16 17:56:26 -0500

    readme

---
+++ webflix/src/test/java/app/webflix/WebflixApplicationTests.java
@@ -0,0 +1,13 @@
+package app.webflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
